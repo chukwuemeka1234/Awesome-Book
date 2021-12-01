1b213d9eb0994635d3b71b8bdcb07b0b301e4aa4
oluwajuwon8 <azeezatlola@gmail.com>
2021-12-01 10:32:24 +0100

    Add code to css

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ class Book {
     this.isbn = isbn;
   }
 }
+
+/* eslint max-classes-per-file: off */
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach((book, index) => {
+      if (book.isbn !== isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
